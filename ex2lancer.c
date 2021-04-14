3be0d705f72475bf89433dc2657ff4ecdef10065
Nathan-ROUCHE <nathan.rouche@etu.umontpellier.fr>
2021-04-14 09:12:03 +0200

    ex2lancer.c

--- ex2lancer.c
+++ ex2lancer.c
@@ -6,4 +6,29 @@
 
 int main(int argc, char **argv) {
 
+    int pid = fork();
+    if(pid == 0)
+    {
+        // Fils
+        char *args[argc];
+
+        for(int i = 1; i < argc; i++)
+        {
+            args[i-1] = argv[i];
+        }
+        args[argc-1] = NULL;
+
+        execvp(argv[1], args);
+
+        perror(argv[1]);
+        exit(1);
+    }
+    else
+    {
+        // Pere
+        int status;
+        waitpid(pid, &status, 0);
+        printf("The END");
+    }
+
 }
