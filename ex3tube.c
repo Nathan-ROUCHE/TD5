a98919cdc83dc1ebd5d4cee48f6a1b128791506c
Nathan-ROUCHE <nathan.rouche@etu.umontpellier.fr>
2021-04-14 09:56:14 +0200

    ex3tube

--- ex3tube.c
+++ ex3tube.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main (int argc, char const **argv) {
+int main(void) {
+  // ls | grep main
 
+  int pid1, pid2;
+  int pipefd[2];
+  pipe(pipefd);
+  // pipefd[0] -> lecture
+  // pipefd[1] -> écriuture
+  
+  pid1 = fork();
+  if (pid1 == 0) {
+    // premier fils (ls)
+    close(1);
+    dup2(pipefd[1], 1);
+    // le descripteur 1 correspond au côté écriture du pipe
+    execlp("ls", "ls", NULL);
+    exit(0);
+  }
+
+  close(pipefd[1]);
+  
+  pid2 = fork();
+  if (pid2 == 0) {
+    // 2e fils (grep main)
+    dup2(pipefd[0], 0);
+    // le descripteur 0 correspond au côté lecture du pipe
+    execlp("grep", "grep", "ex2", NULL);
+    exit(0);
+  }
+
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
 }
